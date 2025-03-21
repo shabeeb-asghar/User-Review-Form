8e24fe231be79220480a798ae416d204bcf84907
Mir Hussain <mirhussainjan10387@gmail.com>
2025-03-22 01:14:09 +0500

    Rendering Multiple Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router";
 import FormText from "./FormText";
 import RatingSubmitMelbourne from "./RatingSubmitMelbourne";
 import RatingSubmitGeelong from "./RatingSubmitGeelong";
